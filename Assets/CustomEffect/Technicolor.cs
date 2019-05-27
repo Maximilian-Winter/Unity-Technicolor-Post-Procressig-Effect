0effce464c17141756c1ce20e1210b545c90619f
Maximilian Winter <maximilian.winter.91@gmail.com>
2019-05-28 00:12:19 +0200

    Adding shader files

---
+++ Assets/CustomEffect/Technicolor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[Serializable]
+[PostProcess(typeof(TechnicolorRenderer), PostProcessEvent.AfterStack, "Custom/Technicolor")]
+public sealed class Technicolor : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Technicolor effect intensity.")]
+    public FloatParameter techniAmount = new FloatParameter { value = 0.5f };
+
+    [Range(0f, 128f), Tooltip("Technicolor power.")]
+    public IntParameter techniPower = new IntParameter { value = 0 };
+
+    [Range(0f, 1f), Tooltip("Technicolor red negative intensity.")]
+    public FloatParameter redNegativeAmount = new FloatParameter { value = 0.5f };
+
+    [Range(0f, 1f), Tooltip("Technicolor green negative intensity.")]
+    public FloatParameter greenNegativeAmount = new FloatParameter { value = 0.5f };
+
+    [Range(0f, 1f), Tooltip("Technicolor blue negative intensity.")]
+    public FloatParameter blueNegativeAmount = new FloatParameter { value = 0.5f };
+}
+
+public sealed class TechnicolorRenderer : PostProcessEffectRenderer<Technicolor>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Technicolor"));
+        sheet.properties.SetFloat("_techniAmount", settings.techniAmount);
+        sheet.properties.SetFloat("_techniPower", settings.techniPower);
+        sheet.properties.SetFloat("_redNegativeAmount", settings.redNegativeAmount);
+        sheet.properties.SetFloat("_greenNegativeAmount", settings.greenNegativeAmount);
+        sheet.properties.SetFloat("_blueNegativeAmount", settings.blueNegativeAmount);
+
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ Assets/CustomEffect/Technicolor.shader
@@ -0,0 +1,60 @@
+﻿Shader "Hidden/Custom/Technicolor"
+{
+	HLSLINCLUDE
+
+	#include "..\PostProcessing/Shaders/StdLib.hlsl"
+	TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+	float _techniAmount;
+	int _techniPower;
+	float _redNegativeAmount;
+	float _greenNegativeAmount;
+	float _blueNegativeAmount;
+
+	#define cyanfilter float3(0.0, 1.30, 1.0)
+	#define magentafilter float3(1.0, 0.0, 1.05) 
+	#define yellowfilter float3(1.6, 1.6, 0.05)
+
+	#define redorangefilter float2(1.05, 0.620) //RG_
+	#define greenfilter float2(0.30, 1.0)       //RG_
+	#define magentafilter2 magentafilter.rb     //R_B
+
+	float4 Frag(VaryingsDefault i) : SV_Target
+	{
+		float4 colorInput = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+		
+		float3 tcol = colorInput.rgb;
+		
+		float2 rednegative_mul = tcol.rg * (1.0 / (_redNegativeAmount * _techniPower));
+		float2 greennegative_mul = tcol.rg * (1.0 / (_greenNegativeAmount * _techniPower));
+		float2 bluenegative_mul = tcol.rb * (1.0 / (_blueNegativeAmount * _techniPower));
+
+		float rednegative = dot(redorangefilter, rednegative_mul);
+		float greennegative = dot(greenfilter, greennegative_mul);
+		float bluenegative = dot(magentafilter2, bluenegative_mul);
+
+		float3 redoutput = rednegative.rrr + cyanfilter;
+		float3 greenoutput = greennegative.rrr + magentafilter;
+		float3 blueoutput = bluenegative.rrr + yellowfilter;
+
+		float3 result = redoutput * greenoutput * blueoutput;
+		colorInput.rgb = lerp(tcol, result, _techniAmount);
+		
+		return colorInput;
+	}
+		ENDHLSL
+
+		SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+			Pass
+		{
+			HLSLPROGRAM
+
+			#pragma vertex VertDefault
+			#pragma fragment Frag
+
+			ENDHLSL
+		}
+	}
+}
